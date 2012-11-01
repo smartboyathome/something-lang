bb00802021553231c911551c2e227ea3dd046767
Smartboy <smartboyathome@gmail.com>
2012-11-01 15:54:14 -0700

    More Scopes

--- Scopes.h
+++ Scopes.h
@@ -2,22 +2,18 @@
 #include <map>
 #include <stack>
 #include <string>
+#include "IdentList/Type.h"
 using namespace std;
-
-class Object
-{
-
-};
 
 class LocalScope
 {
 private:
-    Map<string, Object*> parent_scope;
-    Map<string, Object*> local_scope;
+    Map<string, Type*> parent_scope;
+    Map<string, Type*> local_scope;
 public:
     bool IsInScope(string);
-    bool Insert(string, Object*);
-    bool Modify(string, Object*);
+    bool Insert(string, Type*);
+    bool Modify(string, Type*);
     bool Remove(string);
 };
 
