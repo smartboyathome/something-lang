5c67e779873dcd1e5db683f8495ca73b8c83c844
tie23 <kyle.geib@gmail.com>
2012-11-01 17:22:50 -0700

    More work for Variable. Adding Constant, which is very similar to the Variable.

---
+++ IdentTypes/Constant.cpp
@@ -0,0 +1,29 @@
+// Constant.cpp
+#include "Constant.h"
+
+// Constructor
+// Takes a string name and string value
+Constant::Constant(const string name, const string value) {
+	identifier = name;
+	constant_value = value;
+}
+
+// Destructor
+Constant::~Constant() {
+}
+
+// Get the value
+string Constant::GetValue() {
+	return constant_value;
+}
+
+// Return a string representation of this object
+string Constant::ToString() const {
+	return identifier + " " + constant_value;
+}
+
+// Return a C-formatted string representation of this object
+string Constant::CString() const {
+
+}
+	
